453a85ab65c7c65821943c84f110eee89a60fdd8
sonamng <86510537+sonamng@users.noreply.github.com>
2022-03-11 15:10:22 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,8 @@
+
+
+function addNumber(){
+    var value1=parseInt(document.getElementById("val1").value);
+    var value2=parseInt(document.getElementById("val2").value);
+    var ans=document.getElementById("answer")
+    ans.value=value1+value2;  
+}
